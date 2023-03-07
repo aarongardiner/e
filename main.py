772b509ba9debd5f290d532b41274a130ab3c976
Aaron Gardiner <126822271+aarongardiner@users.noreply.github.com>
2023-03-07 00:17:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+names = ["bob", "fred", "ollie"] #names in a variable
+for x in names:# making names = x
+  print(x) #printing x (names)
+
+for i in range(1, 10,2):
+    print(i, i ** 2)
+print('end of loop')
